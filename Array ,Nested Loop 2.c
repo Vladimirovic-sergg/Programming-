4903bd443586f82b47536685cc0915d88007291e
Vladimirovic-sergg <vladimirovichsergon@zetech.ac.ke>
2024-11-14 23:48:03 +0300

    Create Array ,Nested Loop 2.c

---
+++ Array ,Nested Loop 2.c
@@ -0,0 +1,15 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int i,j;
+    int scores [2] [2]={{84,72},{59,67}} ;
+    
+    for(i=0;i<2;i++) {
+        for(j=0;j<2;j++){
+       printf("%d",scores[i][j]);  
+    }
+    printf("\n");
+    }
+    return 0;
+}
