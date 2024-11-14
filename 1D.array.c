b296364a75342bef915eb6e932a827b2efd7c62c
Vladimirovic-sergg <vladimirovichsergon@zetech.ac.ke>
2024-11-14 13:41:28 +0300

    Create 1D.array.c

---
+++ 1D.array.c
@@ -0,0 +1,11 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+int main(){
+    int i;
+    int marks(6)=[40,50,60,70,80,90];
+    for(i=0;i<6;i++){
+        printf("%d\n",&marks[i]);
+    }
+
+return 0;
+}
