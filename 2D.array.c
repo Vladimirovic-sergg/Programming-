246938b73b7ca2766ef5bee74ffb3211f5726626
Vladimirovic-sergg <vladimirovichsergon@zetech.ac.ke>
2024-11-14 13:46:49 +0300

    Create 2D.array.c

---
+++ 2D.array.c
@@ -0,0 +1,13 @@
+int main(){
+    int i,j;
+    int marks[2][3]= {{40,50,60},{70,80,90}};
+    for(i=0;i<2;i++) {
+        for(j=0;j<3;j++) {
+            
+        }
+    }
+  printf("%d \n",marks (i)(j); }
+  printf("marks(%d)(%d) = %d \n",i,j,marks(i)(j);
+}
+
+return 0;
