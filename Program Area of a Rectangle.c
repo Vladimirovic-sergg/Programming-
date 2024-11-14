0424ae3678bb8e6fafcc451803b54ba20f916ae4
Vladimirovic-sergg <vladimirovichsergon@zetech.ac.ke>
2024-11-14 23:27:08 +0300

    Create Program Area of a Rectangle.c

---
+++ Program Area of a Rectangle.c
@@ -0,0 +1,15 @@
+//program to find area of a rectangle
+#include <stdio.h> //scanf() printf()
+
+int main () {
+    int L;
+    int W;
+    int Area;
+
+    printf("Enter the Length");
+    scanf("%d",&L);
+    printf("Enter the Width");
+
+    Area = L * W; //dec and initialization
+    printf("Area of L*W is %d",Area);
+    return 0;
