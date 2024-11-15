770d18a29ea6f3832ce7d996d5b706053268c6b9
Vladimirovic-sergg/Programming.c <vladimirovichsergon@zetech.ac.ke>
2024-11-15 21:06:30 +0300

    Update 2D.array.c

--- 2D.array.c
+++ 2D.array.c
@@ -1,13 +1,16 @@
-int main(){
-    int i,j;
-    int marks[2][3]= {{40,50,60},{70,80,90}};
-    for(i=0;i<2;i++) {
-        for(j=0;j<3;j++) {
-            
+#include <stdio.h>
+
+int main() {
+    int i, j;
+    int marks[2][3] = {{40, 50, 60}, {70, 80, 90}};
+
+for (i = 0; i < 2; i++) {
+for (j = 0; j < 3; j++) {
+printf("%d ", marks[i][j]);
         }
+ printf("\n");
     }
-  printf("%d \n",marks (i)(j); }
-  printf("marks(%d)(%d) = %d \n",i,j,marks(i)(j);
-}
+printf("marks[1][2] = %d\n",marks(i)(j) );
 
 return 0;
+}
