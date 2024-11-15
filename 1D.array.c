4daa33c716fce39d897b3e755464df9853ebb954
Vladimirovic-sergg/Programming.c <vladimirovichsergon@zetech.ac.ke>
2024-11-15 20:56:05 +0300

    Update 1D.array.c

--- 1D.array.c
+++ 1D.array.c
@@ -1,11 +1,12 @@
-// Online C compiler to run C program online
 #include <stdio.h>
-int main(){
+
+int main() {
     int i;
-    int marks(6)=[40,50,60,70,80,90];
-    for(i=0;i<6;i++){
-        printf("%d\n",&marks[i]);
+    int marks[6] = {40, 50, 60, 70, 80, 90};
+
+    for (i = 0; i < 6; i++) {
+        printf("%d\n", marks[i]); // Print the value, not the address
     }
 
-return 0;
+    return 0;
 }
